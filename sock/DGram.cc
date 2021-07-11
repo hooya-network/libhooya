b0fd0189ec52c806bba8998e3e3ed8fbb2dfa3e8
Wesley Coakley <w@wesleycoakley.com>
2021-07-11 00:26:49 -0400

    Network-order byte handling and uint8_t as primitive bytes

--- sock/DGram.cc
+++ sock/DGram.cc
@@ -1,22 +1,56 @@
 #include "hooya/sock/DGram.hh"
 
+#include <arpa/inet.h>
 #include <cstring>
 #include "hooya/sock/Exception.hh"
 
 namespace hooya::sock {
-bool DGram::Parse(const char *d, size_t len) {
-	const size_t headerSize = sizeof(header);
-	const std::byte *const headerStart = (std::byte*)d;
-	const std::byte *const payloadEnd = (std::byte*)d + len;
-	const std::byte *headerBoundary = headerStart + headerSize;
+bool DGram::Parse(const uint8_t *d, size_t len) {
+	const size_t pOffset = sizeof(DGramHeader_t);
 
 	// Header cannot be larger than the datagram itself lol
-	if (headerSize >= len)
+	if (pOffset >= len)
 		throw ParseException("Datagram too small to parse");
 
 	// Extract header and payload raw data
-	std::memcpy(&header, d, headerSize);
-	payload = std::vector<std::byte>(headerBoundary, payloadEnd);
+	std::memcpy(&header, d, pOffset);
+
+	// Interpret network-order longs and shorts
+	header.Magic = ntohl(header.Magic);
+	header.Version = ntohs(header.Version);
+	header.Reserved = ntohs(header.Reserved);
+
+	// Don't accept packets with mismatching magic numbers
+	if (header.Magic != MAGIC) {
+		throw ParseException("Magic numbers do not match");
+	}
+
+	payload.assign(d + pOffset, d + len);
+
+	if (header.Version != CURRENTVERSION)
+		throw ParseException("Datagram format version mismatch");
 
 	return true;
+}
+
+bool DGram::Parse(const std::vector<uint8_t> &d) {
+	return Parse(d.data(), d.size());
+}
+
+void DGram::Payload(const std::vector<uint8_t> &p) {
+	payload = p;
+}
+
+void DGram::Payload(const std::string &s) {
+	payload = std::vector<uint8_t>(s.begin(), s.end());
+}
+
+std::vector<uint8_t> DGram::Payload() {
+	return payload;
+}
+
+void DGram::Header(int offset, int contextlen) {
+	header.ContextLen = contextlen;
+	header.DataOffset = offset;
+	header.Magic = MAGIC;
 } }
--- sock/include/hooya/sock/DGram.hh
+++ sock/include/hooya/sock/DGram.hh
@@ -5,9 +5,34 @@
 #include <stdexcept>
 
 namespace hooya::sock {
+/**
+ * Header describing the context of the payload within the larger transaction
+ */
 typedef struct DGramHeader {
 public:
-	uint32_t DataLen : 32;
+	/**
+	 * Magic number
+	 */
+	uint32_t Magic : 32;
+
+	/**
+	 * Datagram format version
+	 */
+	uint16_t Version : 16;
+
+	/**
+	 * Reserved
+	 */
+	uint16_t Reserved : 16;
+
+	/**
+	 * Length of the transaction of which this payload is a part of
+	 */
+	uint32_t ContextLen : 32;
+
+	/**
+	 * Offset of the payload within the larger transaction stream
+	 */
 	uint32_t DataOffset : 32;
 } DGramHeader_t;
 
@@ -18,7 +43,29 @@ public:
 	 * \param d Raw network-order data from a socket
 	 * \param len Length of data
 	 */
-	bool Parse(const char *d, size_t len);
+	bool Parse(const uint8_t *d, size_t len);
+
+	bool Parse(const std::vector<uint8_t> &d);
+
+	/**
+	 * Sets the payload of this DGram (for sending)
+	 * \param p Payload data
+	 *
+	 * TODO Would this make more sense to pass in as a stream?
+	 */
+	void Payload(const std::vector<uint8_t> &p);
+
+	void Payload(const std::string &s);
+
+	std::vector<uint8_t> Payload();
+
+	/**
+	 * Sets the header information conveyed in this DGram (for sending)
+	 * \param offset Offset of the payload in the context of the data stream
+	 * \param contextlen Combined length of all payloads of packets in this
+	 * transaction
+	 */
+	void Header(int offset, int contextlen);
 private:
 	/**
 	 * Datagram header comes first in the datagram and describes the payload and
@@ -29,5 +76,12 @@ private:
 	/**
 	 * The datagram payload comes after the header
 	 */
-	std::vector<std::byte> payload;
+	std::vector<uint8_t> payload;
+
+	/**
+	 * Magic header number
+	 */
+	const uint32_t MAGIC = 0x68596121;
+
+	const uint32_t CURRENTVERSION = 0x00;
 }; }
